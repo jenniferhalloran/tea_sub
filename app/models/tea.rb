d5dccd75e449e086321ebcd0f14c80d8c84fa544
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-09-20 15:59:57 -0400

    Create tea model

---
+++ app/models/tea.rb
@@ -0,0 +1,5 @@
+class Tea < ApplicationRecord 
+  has_many :subscriptions 
+  has_many :customers, through: :subscriptions 
+  validates_presence_of :title, :description, :temperature, :brew_time
+end
