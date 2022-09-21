86726a4ee6d710731374a08cead912e8e10e5029
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-09-21 18:58:28 -0400

    Update user subscriptions serializer

--- app/controllers/api/v1/user_subscriptions_controller.rb
+++ app/controllers/api/v1/user_subscriptions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UserSubscriptionsController < ApplicationController
       customer = Customer.find(params[:id])
       render json: UserSubscriptionsSerializer.new(customer), status: 200
     rescue ActiveRecord::RecordNotFound => e  
-    serialize_error(e)
+      serialize_error(e)
     end
   end
 end
--- app/serializers/user_subscriptions_serializer.rb
+++ app/serializers/user_subscriptions_serializer.rb
@@ -3,9 +3,4 @@ class UserSubscriptionsSerializer
  
   attributes :first_name, :last_name, :email, :street, :zip_code, :state, :city, :subscriptions
 
-  def subscriptions 
-    object.subscriptions.collect do |sub|
-      SubscriptionSerializer.new(sub)
-    end
-  end
 end
